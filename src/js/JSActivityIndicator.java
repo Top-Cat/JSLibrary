614146ea8a9b6a6d17fc3e129b4cc05b87412da8
Josh Sunshine <josh@joshsunshine.me.uk>
2012-04-05 10:51:23 +0100

    Added documentation to JSActivityIndicator

--- src/js/JSActivityIndicator.java
+++ src/js/JSActivityIndicator.java
@@ -8,33 +8,55 @@ import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
+/**
+ * JSActivityIndicator allows a spinner to be displayed when invisible processing is occurring. It is modelled on the spinner
+ * seen in Mac OS X and iOS. The size of the spinner is always 36 by 36 pixels.
+ * 
+ * @author Josh
+ *
+ * @version	1.0
+ */
 public class JSActivityIndicator extends JComponent {
 
-	private boolean animating = false;
-	private JLabel label = new JLabel();
+	private JLabel label;
 	
+	/**
+	 * Creates a new spinner but doesn't start animating it yet.
+	 */
 	public JSActivityIndicator() {
-		
+		label = new JLabel();
 	}
 	
+	/**
+	 * Displays the spinner and starts the animation.
+	 */
 	public void startAnimating() {
-		animating = true;
 		label.setIcon(new ImageIcon("images/ActivityIndicator.gif"));
 		label.setBounds(0, 0, 36, 36);
 		add(label);
 		repaint();
 	}
 	
+	/**
+	 * Stops the animation and removes the spinner from view.
+	 */
 	public void stopAnimating() {
-		animating = false;
 		remove(label);
 		repaint();
 	}
 	
+	/**
+	 * <b>This method does nothing when called</b>. It is overridden to cause the size of the spinner
+	 * to always be 36 by 36. 
+	 */
 	public void setSize(int width, int height) {
 		super.setSize(36, 36);
 	}
 	
+	/**
+	 * Allows the location of the spinner to be set, but the width and height parameters are
+	 * overridden to cause the size of the spinner to always be 36 by 36.
+	 */
 	public void setBounds(int x, int y, int width, int height) {
 		super.setBounds(x, y, 36, 36);
 	}
