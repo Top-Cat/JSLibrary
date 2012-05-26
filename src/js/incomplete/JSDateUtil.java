aa905cb17bd99adf1a772c6e56155c7415fbc54c
Josh Sunshine <josh@joshsunshine.me.uk>
2012-05-26 09:16:38 +0100

    Started getRelativeDate (JSDateUtil)

--- src/js/incomplete/JSDateUtil.java
+++ src/js/incomplete/JSDateUtil.java
@@ -1,5 +1,12 @@
 package js.incomplete;
 
+import java.util.Calendar;
+
 public class JSDateUtil {
 
+	public String getRelativeDate(Calendar date) {
+		Calendar now = Calendar.getInstance();
+		
+	}
+	
 }
