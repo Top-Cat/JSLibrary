3ffe815e373535a68714ec92ff04773c93f4be57
Josh Sunshine <josh@joshsunshine.me.uk>
2012-05-26 16:54:24 +0100

    Finished getRelativeDate (JSDateUtil)

--- src/js/incomplete/JSDateUtil.java
+++ src/js/incomplete/JSDateUtil.java
@@ -4,9 +4,18 @@ import java.util.Calendar;
 
 public class JSDateUtil {
 
-	public String getRelativeDate(Calendar date) {
+	public static String getRelativeDate(Calendar date) {
 		Calendar now = Calendar.getInstance();
+		long diff = now.getTimeInMillis() - date.getTimeInMillis();
+		long days = diff / 86400000;
 		
+		if (days < 0) {
+			return "in " + Math.abs(days) + " days";
+		} else if (days > 0) {
+			return days + " days ago";
+		} else {
+			return "today";
+		}
 	}
 	
 }
