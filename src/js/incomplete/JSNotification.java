7d3a58423598b234fbd10754cd2c17924f96d6ba
Josh Sunshine <josh@joshsunshine.me.uk>
2012-07-18 13:53:27 +0100

    Added positioning constants

--- src/js/incomplete/JSNotification.java
+++ src/js/incomplete/JSNotification.java
@@ -14,6 +14,16 @@ public class JSNotification extends JFrame {
 	private JLabel textLabel;
 	private JLabel iconLabel;
 	
+	public static final int TOP_LEFT = 0;
+	public static final int TOP = 1;
+	public static final int TOP_RIGHT = 2;
+	public static final int LEFT = 3;
+	public static final int CENTER = 4;
+	public static final int RIGHT = 5;
+	public static final int BOTTOM_LEFT = 6;
+	public static final int BOTTOM = 7;
+	public static final int BOTTOM_RIGHT = 8;
+	
 	public JSNotification(String text) {
 		this("", text, null);
 	}
