91f1463a98044d0e7dc1c2e1b423bffc30d92649
Josh Asch <josh@josh-asch.net>
2012-07-27 09:57:43 +0100

    Added vectors for components and labels

--- src/js/incomplete/JSEntryDialog.java
+++ src/js/incomplete/JSEntryDialog.java
@@ -1,7 +1,19 @@
 package js.incomplete;
 
+import java.util.Vector;
+
+import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 
 public class JSEntryDialog extends JDialog {
 
+	private Vector<JComponent> components;
+	private Vector<JLabel> labels;
+	
+	public JSEntryDialog() {
+		components = new Vector<JComponent>();
+		labels = new Vector<JLabel>();
+	}
+	
 }
