b9e9f83c68ad54e32c1b53023e5c08212762159f
Josh Asch <josh@josh-asch.net>
2012-07-27 11:08:23 +0100

    Added title label

--- src/js/incomplete/JSEntryDialog.java
+++ src/js/incomplete/JSEntryDialog.java
@@ -2,6 +2,7 @@ package js.incomplete;
 
 import java.util.Vector;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -10,10 +11,31 @@ public class JSEntryDialog extends JDialog {
 
 	private Vector<JComponent> components;
 	private Vector<JLabel> labels;
+	private Vector<JButton> buttons;
 	
-	public JSEntryDialog(String title, String[] componentList, String[] buttons) {
+	private JLabel titleLabel;
+	
+	private int y;
+	
+	public JSEntryDialog(String title, String[] componentList, String[] buttonList) {
 		components = new Vector<JComponent>();
 		labels = new Vector<JLabel>();
+		buttons = new Vector<JButton>();
+		
+		setTitle(title);
+		setLayout(null);
+		setSize(400, 400);
+		
+		titleLabel = new JLabel(title, JLabel.CENTER);
+		titleLabel.setFont(titleLabel.getFont().deriveFont(18f));
+		titleLabel.setBounds(0, 0, 400, 30);
+		add(titleLabel);
 	}
+	
+	public JComponent[] getComponents() {
+		return components.toArray(new JComponent[components.size()]);
+	}
+	
+	
 	
 }
