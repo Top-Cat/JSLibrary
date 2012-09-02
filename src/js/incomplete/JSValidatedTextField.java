463a3fc4febaaadda082a652c039f042c20a1f63
Josh Asch <josh@josh-asch.net>
2012-09-02 21:17:01 +0100

    Created ValidatedTextField including class variables

---
+++ src/js/incomplete/JSValidatedTextField.java
@@ -0,0 +1,18 @@
+package js.incomplete;
+
+import javax.swing.JTextField;
+
+public class JSValidatedTextField extends JTextField {
+
+	public static final int PRESENCE_CHECK = 0;
+	public static final int LENGTH_CHECK = 1;
+	public static final int RANGE_CHECK = 2;
+	public static final int FORMAT_CHECK = 3;
+	
+	private int minLength;
+	private int maxLength;
+	private double minValue;
+	private double maxValue;
+	private String pattern;
+	
+}
