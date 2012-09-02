d067b5ccbea626f6e2a2b3c05dcf7a1ac8b5f41e
Josh Asch <josh@josh-asch.net>
2012-09-02 23:36:17 +0100

    Made constants private

--- src/js/incomplete/JSValidatedTextField.java
+++ src/js/incomplete/JSValidatedTextField.java
@@ -4,10 +4,10 @@ import javax.swing.JTextField;
 
 public class JSValidatedTextField extends JTextField {
 
-	public static final int PRESENCE_CHECK = 0;
-	public static final int LENGTH_CHECK = 1;
-	public static final int RANGE_CHECK = 2;
-	public static final int FORMAT_CHECK = 3;
+	private static final int PRESENCE_CHECK = 0;
+	private static final int LENGTH_CHECK = 1;
+	private static final int RANGE_CHECK = 2;
+	private static final int FORMAT_CHECK = 3;
 	
 	private int type;
 	private int minLength;
