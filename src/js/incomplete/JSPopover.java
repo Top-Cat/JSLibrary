d54f386e363e0c195b7e947906034d1ee9cec75d
Josh Asch <josh@josh-asch.net>
2012-09-03 21:28:39 +0100

    Added paint method

--- src/js/incomplete/JSPopover.java
+++ src/js/incomplete/JSPopover.java
@@ -1,7 +1,36 @@
 package js.incomplete;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class JSPopover extends JFrame {
+	
+	private Color strokeColor = Color.black;
+	private JPanel panel;
+	
+	public void paint(Graphics g) {				
+		g.setColor(strokeColor);
+		
+		int w = getWidth();
+		int h = getHeight();
+		int mH = h / 2;
+		int mW = w / 2;
+		
+		int[] xPoints = {0, 20, 20, 0};
+		int[] yPoints = {mH, mH - 20, mH + 20, mH};
+		int nPoints = xPoints.length;
+		
+		Polygon p = new Polygon(xPoints, yPoints, nPoints);
+		g.fillPolygon(p);
+		g.fillRoundRect(20, 0, w - 21, h - 1, 20, 20);
+		
+		panel.setBackground(Color.RED);
+		add(panel);
+		super.paint(g);
+	}
 
 }
